4679ff295260179c2c7c8b95b189afaaf7214ec3
EvelinaClayton <clayton.evelina@gmail.com>
2019-07-29 16:31:59 +0100

    refactored method top_up to accept @balance and amount

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,7 +6,11 @@ describe Oystercard do
     end
 
 describe '#top_up' do 
-  it { is expected.to respond_to(:top_up).with(1).argument }
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  it 'can top up the balance' do 
+    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+  end   
 end 
-    
+
 end
