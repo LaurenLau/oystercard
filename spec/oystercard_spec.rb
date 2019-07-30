64dbe6744e7272ebaa9559ea5ae37dce51edf8f6
Laurenlau <harris_lau@hotmail.co.uk>
2019-07-30 16:50:37 +0100

    Add test for deducting fair

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,18 +2,18 @@ require 'oystercard'
 
 describe Oystercard do
 
-max_balance = Oystercard::MAXIMUM_BALANCE
-min_balance = Oystercard::MINIMUM_BALANCE
+  max_balance = Oystercard::MAXIMUM_BALANCE
+  min_balance = Oystercard::MINIMUM_BALANCE
 
-    it 'has a balance of zero' do
-      expect(subject.balance).to eq(0)
-    end
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
 
   describe '#top_up' do 
     it 'can top up the balance' do 
       expect{ subject.top_up(min_balance) }.to change{ subject.balance }.by(min_balance)
     end
-
+  
     it 'raises an error when top up limit reached' do
       subject.top_up(max_balance)
       expect { subject.top_up(min_balance) }.to raise_error "Maximum balance of #{max_balance} exceeded"
@@ -29,6 +29,13 @@ min_balance = Oystercard::MINIMUM_BALANCE
   describe '#touch_in' do
     it 'has a minimum balance to touch_in' do
       expect{ subject.touch_in }.to raise_error "Please top up to touch in"
+    end
+  end
+
+  describe '#touch_out' do
+    it 'deducts fare upon touching out' do
+      subject.top_up(max_balance)
+      expect{ subject.touch_out }.to change{ subject.balance }.by(-min_balance)
     end
   end
 
@@ -48,4 +55,5 @@ min_balance = Oystercard::MINIMUM_BALANCE
       expect(subject.in_journey).to eq false
     end
   end
+
 end
