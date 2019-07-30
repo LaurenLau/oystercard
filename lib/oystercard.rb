28c4556bcb48a65253c5b910a33400555047d393
Laurenlau <harris_lau@hotmail.co.uk>
2019-07-30 16:12:00 +0100

    Magic number for min balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,7 +2,7 @@
 class Oystercard
 
   MAXIMUM_BALANCE = 90
-
+  MINIMUM_BALANCE = 1
   attr_reader :balance, :in_journey
 
   def initialize
@@ -24,6 +24,7 @@ class Oystercard
   end
   
   def touch_in
+    fail "Please top up to touch in" if balance < MINIMUM_BALANCE
     @in_journey = true
   end
 
