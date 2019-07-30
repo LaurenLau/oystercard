51a3530600d3d302f534b12088fb4c38d2707ac4
Laurenlau <harris_lau@hotmail.co.uk>
2019-07-30 15:53:44 +0100

    Code created to check oystercard is in journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,10 +3,11 @@ class Oystercard
 
   MAXIMUM_BALANCE = 90
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -19,7 +20,15 @@ class Oystercard
   end
   
   def in_journey?
-    false
+    @in_journey
   end
   
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
 end
