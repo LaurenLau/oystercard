ac3cabc1b60364dd9803a6bbe23203040ba70b63
Laurenlau <harris_lau@hotmail.co.uk>
2019-07-31 18:39:54 +0100

    Test to check station.rb

---
+++ spec/station_spec.rb
@@ -0,0 +1,15 @@
+require 'station'
+
+describe Station do
+
+  subject { described_class.new(name: "Algate East", zone: 1)}
+
+  it 'has a name' do
+    expect(subject.name).to eq("Algate East")
+  end
+
+  it 'has a zone' do
+    expect(subject.zone).to eq(1)
+  end
+  
+end
