a47d2b9a6c0c7ee7945e02f2c2d0b9f0e424dbb0
Laurenlau <harris_lau@hotmail.co.uk>
2019-08-01 15:52:29 +0100

    Simple test for journey object

---
+++ lib/journey.rb
@@ -0,0 +1,26 @@
+class Journey
+  
+  attr_reader :entry_station, :exit_station
+
+  def initialize
+    @complete = false
+  end
+
+  def start(station)
+    @entry_station = station
+  end
+
+  def finish(station)
+    @exit_station = station
+    @complete = true
+  end
+
+  def fare
+   2
+  end
+
+  def complete?
+    @complete
+  end
+
+end
---
+++ spec/journey_spec.rb
@@ -0,0 +1,39 @@
+require 'journey'
+
+describe Journey do
+
+  let(:entry_station) { double(:entry_station) }
+  let(:exit_station) { double(:exit_station) }
+
+  describe '#start' do
+    it 'can get an entry station' do
+      subject.start(entry_station)
+      expect(subject.entry_station).to eq(entry_station)
+    end
+  end
+
+  describe '#exit' do
+    it 'can get an exit station' do
+      subject.finish(exit_station)
+      expect(subject.exit_station).to eq(exit_station)
+    end
+  end
+  
+  describe '#fare' do
+    it 'can produce a fare' do
+      expect(subject.fare).to eq(2)
+    end
+  end
+
+  describe '#complete?' do
+    it 'returns false for incomplete journey' do
+      subject.start(entry_station)
+      expect(subject).not_to be_complete
+    end
+    
+    it 'can confirm a journey is complete' do
+      subject.finish(exit_station)      
+      expect(subject).to be_complete
+    end
+  end
+end
