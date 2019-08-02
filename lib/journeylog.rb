4e0928ae9ffcb2d82079d03ee20a6943fc16392b
Laurenlau <harris_lau@hotmail.co.uk>
2019-08-02 13:42:45 +0100

    New Journeylog spec

---
+++ lib/journeylog.rb
@@ -0,0 +1,10 @@
+class JourneyLog
+
+  def initialize
+    @journeys = []
+  end
+
+end
+
+# Single responsibility principle
+# The journeylog is responsible for starting a journey, ending a journey and returning a list of journeys
